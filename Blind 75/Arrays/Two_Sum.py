b13c6d2e0ed64eb34e0e5c696ead490c16e2a8e1
monateja9999 <monateja9999@gmail.com>
2025-06-06 20:13:53 -0700

    Updating Leetcode Solutions

--- Blind 75/Arrays/Two_Sum.py
+++ Blind 75/Arrays/Two_Sum.py
@@ -2,7 +2,7 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         d = {}
         for i in range(len(nums)):
-            if target - nums[i] in d:
-                return [d[target - nums[i]], i]
+            check = target - nums[i] 
+            if check in d:
+                return [d[check],i]
             d[nums[i]] = i
-        return []
