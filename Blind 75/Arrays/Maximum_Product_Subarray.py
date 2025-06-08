5858ee4540e183b20b942fd1d3662b48052cd22b
monateja9999 <monateja9999@gmail.com>
2025-06-08 01:30:50 -0700

    Updating Leetcode Solutions

--- Blind 75/Arrays/Maximum_Product_Subarray.py
+++ Blind 75/Arrays/Maximum_Product_Subarray.py
@@ -1,15 +1,10 @@
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        pre_prod = 1
-        suff_prod = 1
-        ans = -inf
-        n = len(nums)
-        for i in range(n):
-            if pre_prod == 0:
-                pre_prod = 1
-            if suff_prod == 0:
-                suff_prod = 1
-            pre_prod *= nums[i]
-            suff_prod *= nums[n-i-1]
-            ans = max(ans, max(pre_prod, suff_prod))
-        return ans
+        min_prod = max_prod = res = nums[0]
+        for num in nums[1:]:
+            if num < 0:
+                min_prod, max_prod = max_prod, min_prod
+            min_prod = min(num, min_prod * num)
+            max_prod = max(num, max_prod * num)
+            res = max(res, max_prod)
+        return res
