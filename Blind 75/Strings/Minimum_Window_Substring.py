4f2d97e9b553d8ad677bca24c0c53cebc01a3253
monateja9999 <monateja9999@gmail.com>
2025-07-08 03:19:37 -0700

    Updating Leetcode Solutions

--- Blind 75/Strings/Minimum_Window_Substring.py
+++ Blind 75/Strings/Minimum_Window_Substring.py
@@ -1,25 +1,20 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        l = r = 0
-        targetLength = len(t)
-        count = 0
-        minLength = sys.maxsize
-        start = 0
+        left = right = count = start = 0
+        min_len = sys.maxsize
         d = collections.Counter(t)
-        while r < len(s):
-            d[s[r]] -= 1
-            if d[s[r]] >= 0:
+        target = len(t)
+        while right < len(s):
+            d[s[right]] -= 1
+            if d[s[right]] >= 0:
                 count += 1
-                while count == targetLength:
-                    if (r - l + 1) < minLength:
-                        minLength = min(minLength, r - l + 1)
-                        start = l      
-                    d[s[l]] += 1
-                    if(d[s[l]] > 0):
+                while count == target:
+                    if right - left + 1 < min_len:
+                        min_len = right - left + 1
+                        start = left
+                    d[s[left]] +=  1
+                    if d[s[left]] > 0:
                         count -= 1
-                    l += 1        
-            r += 1
-        if minLength < sys.maxsize:
-            return s[start:start+minLength]
-        else:
-            return ""
+                    left += 1                   
+            right += 1
+        return s[start : start + min_len] if min_len < sys.maxsize else ""
