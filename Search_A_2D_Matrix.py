1b6ef0332aad3871c77e4a0985e517ab1e233ef0
Mona Teja Kurakula <monateja9999@gmail.com>
2025-10-21 23:32:39 -0500

    Update Search_A_2D_Matrix.py

--- Search_A_2D_Matrix.py
+++ Search_A_2D_Matrix.py
@@ -1,7 +1,6 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         m, n = len(matrix), len(matrix[0])
-
         left, right = 0, m * n - 1
 
         while left <= right:
