48190c846f2987f5c05a4bc10a85506fbc95a8b1
Mona Teja Kurakula <monateja9999@gmail.com>
2025-11-16 21:00:53 -0600

    Update 141. Linked List Cycle.py

--- Google Prep/141. Linked List Cycle.py
+++ Google Prep/141. Linked List Cycle.py
@@ -3,7 +3,6 @@
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
-
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
         slow = fast = head
